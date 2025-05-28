17597dc09d484614c24657d2970274e4fb7b4c6a
Louis Olivier <72631705+cyrano21@users.noreply.github.com>
2025-05-28 23:17:53 +0200

    Remove duplicate directory loopnet-34708fe0fe8abc593d51d13857eda8049cc0b7c2

--- loopnet-34708fe0fe8abc593d51d13857eda8049cc0b7c2/app/compare/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function CompareLoading() {
-  return (
-    <div className="container mx-auto py-8">
-      <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
-      </div>
-    </div>
-  )
-}
