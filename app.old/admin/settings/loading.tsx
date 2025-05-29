4818b4d83617b037b582b697aca572bb65d76cd3
Louis Olivier <72631705+cyrano21@users.noreply.github.com>
2025-05-29 23:51:16 +0200

    repair api task 7

--- app.old/admin/settings/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
-    </div>
-  )
-}
